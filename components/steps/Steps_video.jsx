f145baad1d7b543b29cbcd71213f63f9088dbde9
amirsafi4034 <amirsaifi4034@gmail.com>
2022-05-25 10:58:55 +0530

    mobile UI

--- components/steps/Steps_video.jsx
+++ components/steps/Steps_video.jsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 export const Steps_video = (props) => {
   return (
     <>
-        
         {/* <video src={props.steps_video.videoCard} width="575" height="380" autoPlay={true} /> */}
         <Image src={props.steps_video.videoCard} width="575" height="380" className="rounded"></Image>
     </>
